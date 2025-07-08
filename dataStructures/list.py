b630e61f6510abe5fcbd68ae5bd268bbc595153c
leffty <brigante2004@yandex.ru>
2025-07-08 10:00:15 +0300

    added "copy", "sort" functions to OneWayLinkedList

--- dataStructures/list.py
+++ dataStructures/list.py
@@ -1,9 +1,18 @@
-class ListNode:
+import copy
+
+
+class OneWayListNode:
     def __init__(self, value: int):
+        """
+        Конструктор объекта узла
+        """
         self.value = value
         self.next = None
 
     def __repr__(self) -> str:
+        """
+        Строковое представление узла
+        """
         return f'Value: {self.value}'
 
 
@@ -16,7 +25,7 @@ class OneWayLinkedList:
         self.tail = None
         self._size = 0
 
-    def addElementFront(self, node: ListNode):
+    def addElementFront(self, node: OneWayListNode):
         """
         Добавление элемента в начало связного списка
         """
@@ -32,7 +41,7 @@ class OneWayLinkedList:
         self._size += 1
         return self
 
-    def addElementBack(self, node: ListNode):
+    def addElementBack(self, node: OneWayListNode):
         """
         Добавление элемента в конец связного списка
         """
@@ -47,13 +56,13 @@ class OneWayLinkedList:
         self._size += 1
         return self
 
-    def size(self,) -> int:
+    def size(self) -> int:
         """
         Размер списка
         """
         return self._size
 
-    def pop(self,) -> ListNode:
+    def pop(self) -> OneWayListNode:
         """
         Удаление первого элемента списка
         """
@@ -61,6 +70,41 @@ class OneWayLinkedList:
         self.head = temp.next
         self._size -= 1
         return temp
+
+    def __swap(
+        self,
+        first_node: OneWayListNode,
+        second_node: OneWayListNode
+    ) -> None:
+        """
+        Поменять местами два элемента
+        """
+        first_node.value, second_node.value = (
+            second_node.value,
+            first_node.value
+        )
+
+    def __sort(self) -> None:
+        """
+        Сортировка элементов списка
+        """
+        temp = self.head
+        for i in range(self.size()):
+            for j in range(i+1, self.size() - 1):
+                if temp.value > temp.next.value:
+                    self.__swap(temp, temp.next)
+                temp = temp.next
+
+    def copy(self):
+        """
+        Копирование значений списка в другой список
+        """
+        new_list = OneWayLinkedList()
+        temp = self.head
+        while temp:
+            new_list.addElementBack(copy.copy(temp))
+            temp = temp.next
+        return new_list
 
     def __repr__(self) -> str:
         """
@@ -77,11 +121,13 @@ class OneWayLinkedList:
 
 if __name__ == '__main__':
     ls = OneWayLinkedList()
-    ls.addElementBack(ListNode(1)).addElementBack(
-        ListNode(2)).addElementBack(ListNode(3)).addElementBack(
-        ListNode(4)).addElementBack(ListNode(5))
-    ls.addElementFront(ListNode(0)).addElementFront(ListNode(-1))
+    ls.addElementBack(OneWayListNode(1)).addElementBack(
+        OneWayListNode(2)).addElementBack(OneWayListNode(3)).addElementBack(
+        OneWayListNode(4)).addElementBack(OneWayListNode(5))
+    ls.addElementFront(OneWayListNode(0)).addElementFront(
+        OneWayListNode(-1)).addElementFront(OneWayListNode(-2))
     print(ls, ls.size())
-    node = ls.pop()
-    print(node)
-    print(ls, ls.size())
+    new_ls = ls.copy()
+    new_ls.pop()
+    new_ls.pop()
+    print(new_ls, new_ls.size())
