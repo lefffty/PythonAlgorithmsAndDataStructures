af8419e217b47b194e16929b19fc4c7053b73278
leffty <brigante2004@yandex.ru>
2025-08-21 21:34:35 +0300

    deletions

--- algorithms/gcd.py
+++
@@ -1,9 +0,0 @@
-def gcd(dividend: int, divisor: int):
-    while divisor:
-        r = dividend % divisor
-        dividend = divisor
-        divisor = r
-    return dividend
-
-
-print(gcd(4851, 3003))
