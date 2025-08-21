5a6c3326a266bb6fef76e48d1b04be3b760c450c
leffty <brigante2004@yandex.ru>
2025-08-21 21:31:42 +0300

    deletions

--- algorithms/bubble_sort.py
+++
@@ -1,9 +0,0 @@
-def bubble_sort(numbers: list):
-    for i in range(0, len(numbers) - 1):
-        for j in range(0, len(numbers) - 1):
-            if numbers[j] >= numbers[j+1]:
-                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
-    return numbers
-
-
-print(bubble_sort([1, 8, 6, 2, 6, 4, 0, 7, 5, 1, 3, 6, 4]))
--- algorithms/selection_sort.py
+++
@@ -1,9 +0,0 @@
-def selection_sort(numbers: list):
-    for i in range(0, len(numbers)):
-        for j in range(i, len(numbers)):
-            if numbers[i] > numbers[j]:
-                numbers[i], numbers[j] = numbers[j], numbers[i]
-    return numbers
-
-
-print(selection_sort([9, 4, 8, 2, 4, 9, 6, 4, 7, 3, 2]))
